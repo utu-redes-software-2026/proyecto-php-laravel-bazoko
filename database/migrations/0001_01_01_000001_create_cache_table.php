f326d4f4ab9ba291545cbd7eaf09bd84716865bb
bazoko <jah56112@gmail.com>
2026-05-27 07:43:33 -0300

    migraciones y verciones solucionado

--- database/migrations/0001_01_01_000001_create_cache_table.php
+++ database/migrations/0001_01_01_000001_create_cache_table.php
@@ -6,35 +6,21 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('cache', function (Blueprint $table) {
             $table->string('key')->primary();
             $table->mediumText('value');
-<<<<<<< HEAD
             $table->integer('expiration')->index();
-=======
-            $table->bigInteger('expiration')->index();
->>>>>>> 120b386cfc5f59aa58be5cfd47c3070a5cee1c31
         });
 
         Schema::create('cache_locks', function (Blueprint $table) {
             $table->string('key')->primary();
             $table->string('owner');
-<<<<<<< HEAD
             $table->integer('expiration')->index();
-=======
-            $table->bigInteger('expiration')->index();
->>>>>>> 120b386cfc5f59aa58be5cfd47c3070a5cee1c31
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('cache');
