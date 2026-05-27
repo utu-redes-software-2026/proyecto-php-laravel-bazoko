c6dabe9fd1ed74d842d32a1496461bb90b07bfbf
bazoko <jah56112@gmail.com>
2026-05-27 08:03:45 -0300

    configurar modelo y rutas de mediciones

--- app/Models/Medicion.php
+++ app/Models/Medicion.php
@@ -3,8 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+/**
+ * permite guardar esos campos desde formularios.
+ */
 class Medicion extends Model
 {
-    //
+    protected $fillable = [
+        'fecha',
+        'turno',
+        'sector',
+        'parametro',
+        'valor',
+        'unidad',
+        'responsable',
+        'observaciones',
+    ];
 }
